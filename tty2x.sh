bd70ebc3bdc7b78305d2b8803f37df17e635790c
venice1200 <43762187+venice1200@users.noreply.github.com>
2022-09-17 18:20:23 +0200

    Update tty2x.sh

--- tty2x.sh
+++ tty2x.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # By venice & ojaksch
-# Simple Version of the tty2x Daemon for other TTY-Display-Projects
+# Universal Version of the tty2x Daemon for other tty2-Projects
 #
 # 2022-09-12 Release
 #
@@ -17,19 +17,19 @@ dbug() {
       echo "---------- tty2x Debuglog ----------" > ${debugfile}
     fi 
     echo "${1}" >> ${debugfile}								# send debug text
-	echo "${1}"												# output debug text to console
+    echo "${1}"										# output debug text to console
   fi
 }
 
 # USB Send-Picture-Data function
 senddata() {
-  echo "${1}" > ${TTYDEV}									# send just the CORENAME with line feed "\n"
+  echo "${1}" > ${TTYDEV}								# send just the CORENAME with line feed "\n"
 }
 
 # ** Main **
-if [ -c "${TTYDEV}" ]; then									# check for tty device
+if [ -c "${TTYDEV}" ]; then								# check for tty device
   dbug "${TTYDEV} detected, setting Parameter: ${BAUDRATE} ${TTYPARAM}."
-  stty -F ${TTYDEV} ${BAUDRATE} ${TTYPARAM}					# set tty parameter
+  stty -F ${TTYDEV} ${BAUDRATE} ${TTYPARAM}						# set tty parameter
   sleep ${WAITSECS}
   if [ "${SENDQWERTZ}" = "yes" ]; then
     dbug "Send QWERTZ as first transmission"
@@ -38,25 +38,25 @@ if [ -c "${TTYDEV}" ]; then									# check for tty device
   fi
   while true; do											# main loop
     if [ -r ${corenamefile} ]; then							# proceed if file exists and is readable (-r)
-      newcore=$(cat ${corenamefile})						# get CORENAME
+      newcore=$(cat ${corenamefile})							# get CORENAME
       dbug "Read CORENAME: -${newcore}-"
       dbug "Send -${newcore}- to ${TTYDEV}."
-	  if [ "${newcore}" != "${oldcore}" ]; then				# proceed only if Corename has changed
+	  if [ "${newcore}" != "${oldcore}" ]; then					# proceed only if Corename has changed
         senddata "${newcore}"								# the "transmission"
-        if [ "${debug}" = "false" ]; then					# debug mode off
-	      inotifywait -qq -e modify "${corenamefile}"		# some quiteness for inotify
+        if [ "${debug}" = "false" ]; then						# debug mode off
+	      inotifywait -qq -e modify "${corenamefile}"				# some quiteness for inotify
         fi
-        if [ "${debug}" = "true" ]; then					# debug mode on
-	      inotifywait -e modify "${corenamefile}"			# no quiteness :-)
+        if [ "${debug}" = "true" ]; then						# debug mode on
+	      inotifywait -e modify "${corenamefile}"					# no quiteness :-)
         fi
-		oldcore=${newcore}									# update new corname to "old" variable
+		oldcore=${newcore}							# update new corname to "old" variable
       fi
-    else													# CORENAME file not found
+    else										# CORENAME file not found
      dbug "File ${corenamefile} not found!"
-    fi														# end if /tmp/CORENAME check
-  done														# end while
-else														# no tty detected
+    fi											# end if /tmp/CORENAME check
+  done											# end while
+else											# no tty detected
   echo "No ${TTYDEV} Device detected, abort."
   dbug "No ${TTYDEV} Device detected, abort."
-fi															# end if tty check
+fi											# end if tty check
 # ** End Main **
